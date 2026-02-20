7457fd82c01169008fbde56ab91a9306f73d74b9
lxbxtxmy0 <anton.vasilev@ispring.institute>
2026-02-20 08:42:20 +0300

    first lab

--- lab1/cgi-bin/WorkWithQueryString.pas
+++ lab1/cgi-bin/WorkWithQueryString.pas
@@ -7,20 +7,20 @@ VAR
 FUNCTION GetQueryStringParameter(Key: STRING): STRING;
 VAR
   LeftP, RightP, Start: INTEGER;
-  SubStr: STRING;
+  Temp: STRING;
 BEGIN 
   LeftP := Pos(Key + '=', QueryString);
   IF LeftP <> 0
   THEN
     BEGIN
       Start := LeftP + Length(Key) + 1;
-      SubStr := Copy(QueryString, Start, Length(QueryString));
-      RightP := Pos('&', SubStr);
+      Temp := Copy(QueryString, Start, Length(QueryString));
+      RightP := Pos('&', Temp);
       IF RightP <> 0
       THEN
-        GetQueryStringParameter := Copy(SubStr, 1, RightP - 1)
+        GetQueryStringParameter := Copy(Temp, 1, RightP - 1)
       ELSE
-        GetQueryStringParameter := Copy(SubStr, 1, Length(SubStr))
+        GetQueryStringParameter := Copy(Temp, 1, Length(Temp))
     END
   ELSE
     GetQueryStringParameter := 'parameter ' +  Key + ' does not exist!'
