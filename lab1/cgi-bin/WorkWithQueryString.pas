ff200e1103a73ac16cb3c5ca92437447f8c57816
lxbxtxmy0 <anton.vasilev@ispring.institute>
2026-02-26 13:48:44 +0300

    first lab

--- lab1/cgi-bin/WorkWithQueryString.pas
+++ lab1/cgi-bin/WorkWithQueryString.pas
@@ -1,16 +1,32 @@
 PROGRAM WorkWithQueryString(INPUT, OUTPUT);
 USES
-  DOS;
-
+  DOS;  
+VAR
+  QueryString: STRING;  
 FUNCTION GetQueryStringParameter(Key: STRING): STRING;
 VAR
-
+  StartPos, EndPos: INTEGER;
+  SupStr: STRING;  
 BEGIN
+  StartPos := Pos(Key + '=', QueryString);
+  IF StartPos > 0
+  THEN
+    BEGIN
+      StartPos := StartPos + Length(Key + '=');
+      SupStr := Copy(QueryString, StartPos, Length(QueryString));
+      EndPos := Pos('&', SupStr);
+      IF EndPos > 0 THEN
+        GetQueryStringParameter := Copy(SupStr, 1, EndPos - 1)
+      ELSE
+        GetQueryStringParameter := SupStr
+    END
+END; 
 
-END;
-
-BEGIN {WorkWithQueryString}
+BEGIN 
+  QueryString := GetEnv('QUERY_STRING');
+  WRITELN('Content-Type: text/plain');
+  WRITELN;
   WRITELN('First Name: ', GetQueryStringParameter('first_name'));
   WRITELN('Last Name: ', GetQueryStringParameter('last_name'));
   WRITELN('Age: ', GetQueryStringParameter('age'))
-END. {WorkWithQueryString}
+END.
