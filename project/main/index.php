c750526982a90459effc175f1b5a8f3dce945f18
Anton Vasilev <anton.vasilev@ispring.institute>
2026-04-12 01:15:10 +0300

    init commit

---
+++ project/main/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Home</title>
+</head>
+<body>
+<img src="img/emblem_ispring.svg" alt="ispring" width="96" height="96" class="logo">
+<h1>ПОГРУЖЕНИЕ В ПРОГРАММИРОВАНИЕ</h1>
+<div class="seasons">
+    <a href="#">
+        <img src="img/folder2021.svg" alt="folder" width="320" height="240">
+        <span>СЕЗОН 2021</span>
+    </a>
+    <a href="#">
+        <img src="img/folder2022.svg" alt="folder" width="320" height="240">
+        <span>СЕЗОН 2022</span>
+    </a>
+    <a href="#">
+        <img src="img/folder2023.svg" alt="folder" width="320" height="240">
+        <span>СЕЗОН 2023</span>
+    </a>
+    <a href="#">
+        <img src="img/folder2024.svg" alt="folder" width="320" height="240">
+        <span>СЕЗОН 2024</span>
+    </a>
+    <a href="#">
+        <img src="img/folder2025.svg" alt="folder" width="320" height="240">
+        <span>СЕЗОН 2025</span>
+    </a>
+</div>
+</body>
+</html>
+
+
+
