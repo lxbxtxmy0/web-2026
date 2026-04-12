ead3ec23167fea741af7ed6c1f28cc5166c228a7
Anton Vasilev <anton.vasilev@ispring.institute>
2026-04-12 10:55:44 +0300

    restore

---
+++ lab6/login/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="welcome_side">
+        <h1>Войти</h1>
+        <img src="img/welcome.png" alt="welcome" height="501" width="462">
+    </div>
+    <form class="login">
+        <div class="email">
+            <label for="email-field">Электропочта</label>
+            <input type="email" class="input-field" id="email-field">
+            <p class="example">Введите электропочту в формате *****@***.**</p>
+        </div>
+        <div class="password">
+            <label for="password-field">Пароль</label>
+            <input type="password" class="input-field" id="password-field">
+            <button class="hide_password">
+                <img src="img/eye_off.svg" alt="Скрыть пароль" height="24" width="24">
+            </button>
+        </div>
+        <button class="continue">Продолжить</button>
+    </form>
+</body>
+</html>
---
+++ project-canvas-games/games/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Home</title>
+</head>
+<body>
+<a href="../main" class="back_button">
+    <img src="img/back_arrow.png" alt="стрелка назад" width="160" height="80">
+</a>
+<h1>ИГРЫ</h1>
+<div class="list">
+    <div class="game_field">
+        <img src="img/pp.png" alt="game_preview" width="200" height="200">
+        <div class="info">
+            <span>Игра: змейка</span>
+            <span>Автор: Долгирев Алексей</span>
+        </div>
+        <a href="#">START</a>
+    </div>
+    <div class="game_field">
+        <img src="img/pp.png" alt="game_preview" width="200" height="200">
+        <div class="info">
+            <span>Игра: змейка</span>
+            <span>Автор: Долгирев Алексей</span>
+        </div>
+        <a href="#">START</a>
+    </div>
+<!--...-->
+</div>
+</body>
+</html>
+
+
+
