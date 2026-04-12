527be5455deb6ca3594a546c3f8550ba4f369823
Anton Vasilev <anton.vasilev@ispring.institute>
2026-04-12 03:23:40 +0300

    +games:

--- project/games/index.php
+++ project/games/index.php
@@ -6,8 +6,29 @@
     <title>Home</title>
 </head>
 <body>
-
-
+<a href="../main" class="back_button">
+    <img src="img/back_arrow.png" alt="стрелка назад" width="160" height="80">
+</a>
+<h1>ИГРЫ</h1>
+<div class="list">
+    <div class="game_field">
+        <img src="img/pp.png" alt="game_preview" width="200" height="200">
+        <div class="info">
+            <span>Игра: змейка</span>
+            <span>Автор: Долгирев Алексей</span>
+        </div>
+        <a href="#">START</a>
+    </div>
+    <div class="game_field">
+        <img src="img/pp.png" alt="game_preview" width="200" height="200">
+        <div class="info">
+            <span>Игра: змейка</span>
+            <span>Автор: Долгирев Алексей</span>
+        </div>
+        <a href="#">START</a>
+    </div>
+<!--...-->
+</div>
 </body>
 </html>
 
